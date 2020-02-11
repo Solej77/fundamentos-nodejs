65677df09c6256a7f859d18d1bfc9df7fb6a2149
Solej77 <ing.josgalo@gmail.com>
2020-02-11 16:58:57 -0600

    Building modules: Requiere and Exports

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,4 @@
+const { saludar, prompt1 } = require('./modulo');
+
+saludar();
+console.log(prompt1);
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,8 @@
+function saludar() {
+  console.log('Hola Mundo!!');
+}
+
+module.exports = {
+  saludar,
+  prompt1: 'Promt12',
+};
